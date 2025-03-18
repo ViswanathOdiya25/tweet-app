680a4fafda4e53055e3d9d477b7d08118635195e
viswanath25 <viswanath2507@gmail.com>
2025-03-19 03:30:18 +0530

    auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
-
+import cookkieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookkieParser());
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 5000");
     connectMongoDB();
 });
