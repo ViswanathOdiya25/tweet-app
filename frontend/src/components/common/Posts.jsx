725292632a70604edacf46cacc73c3e92ef6549b
viswanath25 <viswanath2507@gmail.com>
2025-04-07 17:42:48 +0530

    3- Profile page posts and user into fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -12,8 +12,12 @@ const Posts = ({ feedType, username }) => {
                 return "/api/posts/all";
             case "following":
                 return "/api/posts/following";
+            case "posts":
+                return `/api/posts/user/${username}`;
+            case "likes":
+                return `/api/posts/likes/${userId}`;
             default:
-                return "/api/posts/all"
+                return "/api/posts/all";
         }
     };
 
