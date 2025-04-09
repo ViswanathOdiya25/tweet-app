a5c6f02d2ed07c7d77d92de51a5136e4c176b35c
viswanath25 <viswanath2507@gmail.com>
2025-04-10 02:55:16 +0530

    4-route corrected

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -4,24 +4,27 @@ import jwt from "jsonwebtoken";
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
-        if(!token) {return res.status(401).json({error: "Unauthorised: No token"});
+        if (!token) {
+            return res.status(401).json({ error: "Unauthorised: No token" });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        if(!decoded) {return res.status(401).json({error: "Unauthorised: Invalid token"});
+        if (!decoded) {
+            return res.status(401).json({ error: "Unauthorised: Invalid token" });
         }
 
         const user = await User.findById(decoded.userId).select("-password");
 
-        if(!user) {return res.status(404).json({error: "User not found"});
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
         }
 
         req.user = user;
         next();
     } catch (error) {
         console.log("Error in protectRoute middleware", error.message);
-        res.status(500).json({ error: "Internal Server error"});
-        
+        res.status(500).json({ error: "Internal Server error" });
+
     }
 }
--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { protectRoute } from "../middleware/protectroute.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 import { getNotification, deleteNotification } from "../controllers/notification.controller.js";
 
 
 const router = express.Router();
 
-router.get("/", protectRoute,getNotification);
+router.get("/", protectRoute, getNotification);
 router.delete("/", protectRoute, deleteNotification);
 export default router;
